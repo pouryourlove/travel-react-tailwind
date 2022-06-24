8ac376c6917eebffad8bebcf48d5b5ee6837d38f
Juyeon Lee <lqu134@naver.com>
2022-06-24 20:30:01 +0900

    Destination completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Destinations from "./components/Destinations";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Destinations/>
     </div>
   );
 }
