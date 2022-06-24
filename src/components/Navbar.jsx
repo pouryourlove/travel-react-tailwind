bb1c6f84171ba6de628b69b54fd90bce636f235f
Juyeon Lee <lqu134@naver.com>
2022-06-24 20:10:54 +0900

    Hero completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between items-center h-20 px-4">
+    <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white">
       <div>
         <h1 onClick={handleNav} className={logo ? "hidden" : "block"}>
           EUROPE
@@ -51,7 +51,7 @@ const Navbar = () => {
         onClick={handleNav}
         className={
           nav
-            ? "absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col"
+            ? "absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col"
             : "absolute left-[-100%] top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col"
         }
       >
