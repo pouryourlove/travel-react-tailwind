224c4f92f7fc05d569774d01bc2b4c3702dde934
Juyeon Lee <lqu134@naver.com>
2022-06-25 23:52:04 +0900

    slide code changed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,20 +4,19 @@ import { BsArrowRightSquareFill } from "react-icons/bs";
 
 const sliderData = [
   {
-    url: "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bG9uZG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60",
+    url: "https://images.unsplash.com/photo-1515238152791-8216bfdf89a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80",
   },
   {
-    url: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGFyaXN8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60",
+    url: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
   },
   {
-    url: "https://images.unsplash.com/photo-1580339841933-f06ca55842d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3RvY2tob2xtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60",
+    url: "https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
   },
 ];
 
 const Carousel = () => {
   const [slide, setSlide] = useState(0);
   const length = sliderData.length;
-  //   console.log(length);
 
   const prevSlide = () => {
     setSlide(slide === length - 1 ? 0 : slide + 1);
@@ -34,8 +33,9 @@ const Carousel = () => {
       />
       <BsArrowRightSquareFill
         onClick={nextSlide}
-        className="absolute top-[50%] text-3xl text-white cursor-pointer right-8 "
+        className="absolute top-[50%] text-3xl text-white cursor-pointer right-8"
       />
+
       {sliderData.map((item, index) => (
         <div className={index === slide ? "opacity-100" : "opacity-0"}>
           {index === slide && (
---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaPinterest,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="w-full bg-gray-100 py-16">
+      <div className="max-w-[1240px] mx-auto flex flex-col px-4">
+        <div className="sm:flex text-center justify-between items-center">
+          <h1>EUROPE</h1>
+          <div className="flex justify-between w-full sm:max-w-[280px] my-4">
+            <FaFacebook className="icon" />
+            <FaTwitter className="icon" />
+            <FaYoutube className="icon" />
+            <FaPinterest className="icon" />
+            <FaInstagram className="icon" />
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <ul className="lg:flex">
+            <li>about</li>
+            <li>Partnerships</li>
+            <li>Career</li>
+            <li>Newsroom</li>
+            <li>newsroom</li>
+          </ul>
+          <ul className="text-right lg:flex">
+            <li>about</li>
+            <li>Partnerships</li>
+            <li>Career</li>
+            <li>Newsroom</li>
+            <li>newsroom</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
